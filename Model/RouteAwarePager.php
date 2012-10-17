8930ec458e511bc48bd6802f2fd0d330129ce209
Adrien Brault <adrien.brault@gmail.com>
2012-10-17 18:06:19 +0200

    RouteAwarePager is now a real wrapper

--- Model/RouteAwarePager.php
+++ Model/RouteAwarePager.php
@@ -7,7 +7,7 @@ use Pagerfanta\PagerfantaInterface;
 /**
  * A handler is registered for this class. It will add self/next/previous/first/last links.
  */
-class RouteAwarePager
+class RouteAwarePager implements PagerfantaInterface
 {
     protected $pager;
     protected $route;
@@ -33,5 +33,75 @@ class RouteAwarePager
     public function getRouteParameters()
     {
         return $this->routeParameters;
+    }
+
+    public function getIterator()
+    {
+        return $this->pager->getIterator();
+    }
+
+    function setMaxPerPage($maxPerPage)
+    {
+        return $this->pager->setMaxPerPage($maxPerPage);
+    }
+
+    function getMaxPerPage()
+    {
+        return $this->pager->getMaxPerPage();
+    }
+
+    function setCurrentPage($currentPage)
+    {
+        return $this->pager->setCurrentPage($currentPage);
+    }
+
+    function getCurrentPage()
+    {
+        return $this->pager->getCurrentPage();
+    }
+
+    function getCurrentPageResults()
+    {
+        return $this->pager->getCurrentPageResults();
+    }
+
+    function getNbResults()
+    {
+        return $this->pager->getNbResults();
+    }
+
+    function getNbPages()
+    {
+        return $this->pager->getNbPages();
+    }
+
+    function haveToPaginate()
+    {
+        return $this->pager->haveToPaginate();
+    }
+
+    function hasPreviousPage()
+    {
+        return $this->pager->hasPreviousPage();
+    }
+
+    function getPreviousPage()
+    {
+        return $this->pager->getPreviousPage();
+    }
+
+    function hasNextPage()
+    {
+        return $this->pager->hasNextPage();
+    }
+
+    function getNextPage()
+    {
+        return $this->pager->getNextPage();
+    }
+
+    public function count()
+    {
+        return $this->pager->count();
     }
 }
--- Tests/Functional/TestBundle/Model/PostsCollection.php
+++ Tests/Functional/TestBundle/Model/PostsCollection.php
@@ -2,11 +2,13 @@
 
 namespace FSC\HateoasBundle\Tests\Functional\TestBundle\Model;
 
+use Pagerfanta\PagerfantaInterface;
+
 class PostsCollection
 {
     private $pager;
 
-    public function __construct($pager)
+    public function __construct(PagerfantaInterface $pager)
     {
         $this->pager = $pager;
     }
