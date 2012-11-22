b4ef972fa88e9a7c609aa3efd841ae1ebf5121b8
Adrien Brault <adrien.brault@gmail.com>
2012-11-22 20:27:52 +0100

    Remove useless code

--- FSCHateoasBundle.php
+++ FSCHateoasBundle.php
@@ -4,16 +4,7 @@ namespace FSC\HateoasBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use FSC\HateoasBundle\DependencyInjection\FSCHateoasExtension;
-
 class FSCHateoasBundle extends Bundle
 {
-    public function getContainerExtension()
-    {
-        if (null === $this->extension) {
-            $this->extension = new FSCHateoasExtension();
-        }
 
-        return $this->extension;
-    }
 }
