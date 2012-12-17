fc5bf0e35c7b5d9af37deab82dc7e72a983f52d7
Adrien Brault <adrien.brault@gmail.com>
2012-12-17 17:42:30 +0100

    Add addRelation to RelationsManagerInterface

--- Metadata/RelationsManagerInterface.php
+++ Metadata/RelationsManagerInterface.php
@@ -5,4 +5,5 @@ namespace FSC\HateoasBundle\Metadata;
 interface RelationsManagerInterface
 {
     public function addBasicRelations($object, $route = null, $routeParameters = array());
+    public function addRelation($object, $rel, $href, array $embed = null);
 }
