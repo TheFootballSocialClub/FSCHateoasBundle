059c837a1ffa93e52e69ac3b8f08cd09f2d5e21d
Baldur Rensch <baldur.rensch@hautelook.com>
2013-01-23 11:47:43 -0800

    Fix deprecated warnings

--- Factory/ParametersFactory.php
+++ Factory/ParametersFactory.php
@@ -2,7 +2,8 @@
 
 namespace FSC\HateoasBundle\Factory;
 
-use Symfony\Component\Form\Util\PropertyPath;
+use Symfony\Component\PropertyAccess\PropertyPath;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 class ParametersFactory implements ParametersFactoryInterface
 {
@@ -14,8 +15,9 @@ class ParametersFactory implements ParametersFactoryInterface
         $self = $this;
         array_walk($parameters, function (&$value, $key) use ($data, $self) {
             if (is_string($value) && in_array(substr($value, 0, 1), array('.', '['))) {
+                $propertyAccessor = new PropertyAccessor();
                 $propertyPath = new PropertyPath(preg_replace('/^\./', '', $value));
-                $value = $propertyPath->getValue($data);
+                $value = $propertyAccessor->getValue($data, $propertyPath);
 
                 return;
             } elseif ('@' === $value) {
