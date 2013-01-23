e9abe50cb2c9268ed75c9ee7c38888eb9a4a8f48
Baldur Rensch <baldur.rensch@hautelook.com>
2013-01-23 11:47:43 -0800

    Fix build
    Fix deprecated warnings
    
    Fixed broken tests (due to updated dependencies)
    
    Chnges from Code Review
    
    Change type hint to interface
    
    Fixed phpdoc block
    
    Added SF-2.1 and dev-master to build matrix
    
    Use Framework Bundle or FSC PropertyAccessor
    
    Depending on which is available
    
    Fixed config

--- FSCHateoasBundle.php
+++ FSCHateoasBundle.php
@@ -3,8 +3,17 @@
 namespace FSC\HateoasBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use FSC\HateoasBundle\DependencyInjection\Compiler\ConfigurationCheckPass;
 
 class FSCHateoasBundle extends Bundle
 {
-
+	/**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ConfigurationCheckPass());
+    }
 }
