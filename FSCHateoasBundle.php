25d9b1584d5f06938804438a1cbf1cfcd357363e
Baldur Rensch <baldur.rensch@hautelook.com>
2013-01-26 11:47:59 -0800

    Fixed indentation, travis sf version

--- FSCHateoasBundle.php
+++ FSCHateoasBundle.php
@@ -4,12 +4,11 @@ namespace FSC\HateoasBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 use FSC\HateoasBundle\DependencyInjection\Compiler\ConfigurationCheckPass;
 
 class FSCHateoasBundle extends Bundle
 {
-	/**
+    /**
      * {@inheritdoc}
      */
     public function build(ContainerBuilder $container)
