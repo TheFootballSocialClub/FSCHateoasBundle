ec515e6c053580340183648217ea46a69d5fb4ff
Adrien Brault <adrien.brault@gmail.com>
2013-10-07 13:16:14 -0700

    Extract pager links building logic to the RelationsBuilder

--- DependencyInjection/FSCHateoasExtension.php
+++ DependencyInjection/FSCHateoasExtension.php
@@ -30,9 +30,9 @@ class FSCHateoasExtension extends ConfigurableExtension
             ;
         }
 
-        $relationsManagerDefinition = $container->getDefinition('fsc_hateoas.metadata.relations_manager');
-        $relationsManagerDefinition->addArgument($config['pagerfanta']['parameters_name']['page']);
-        $relationsManagerDefinition->addArgument($config['pagerfanta']['parameters_name']['limit']);
+        $relationsBuilderFactoryDefinition = $container->getDefinition('fsc_hateoas.metadata.relation_builder.factory');
+        $relationsBuilderFactoryDefinition->addArgument($config['pagerfanta']['parameters_name']['page']);
+        $relationsBuilderFactoryDefinition->addArgument($config['pagerfanta']['parameters_name']['limit']);
 
         $container->setParameter('fsc_hateoas.json.links_key', $config['json']['links_key']);
         $container->setParameter('fsc_hateoas.json.relations_key', $config['json']['relations_key']);
